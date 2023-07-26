69d83b969db8b18de6dd9c7dbe033be2d7cfbee7
hclo99 <junghs710@naver.com>
2023-07-26 22:54:47 +0900

    feat:postgres 설치

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ContentsModule } from './contents/contents.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './auth/auth.entity';
 
 @Module({
-  imports: [AuthModule, ContentsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'ship99',
+      database: 'EduPlay',
+      entities: [User],
+      synchronize: true,
+    }),
+    AuthModule,
+    ContentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './service/auth.service';
 import { AuthController } from './controller/auth.controller';
+import { User } from './auth.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [AuthService],
   controllers: [AuthController],
 })
