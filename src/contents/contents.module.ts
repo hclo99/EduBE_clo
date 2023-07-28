0d14c68a601f973e10fab6317890b593e890fa65
허지수 <heojisu@heojisuui-MacBookAir.local>
2023-07-28 12:24:30 +0900

    feat: contents 모듈 및 Get API

--- src/contents/contents.module.ts
+++ src/contents/contents.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ContentsService } from './service/contents.service';
 import { ContentsController } from './controller/contents.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Content } from './contents.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Content])],
   providers: [ContentsService],
   controllers: [ContentsController],
 })
--- src/contents/controller/contents.controller.ts
+++ src/contents/controller/contents.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ContentsService } from '../service/contents.service';
 
 @Controller('contents')
-export class ContentsController {}
+export class ContentsController {
+  constructor(private contentsService: ContentsService) {}
+
+  @Get()
+  async getFilesByTopic(@Query('topic') topic: string) {
+    const files = await this.contentsService.findByTopic(topic);
+    console.log('fr:', topic, files);
+    return files.map((file) => ({
+      name: file.name,
+      url: `https://d1gvhezy2xavcm.cloudfront.net/${file.name}.m3u8`,
+      length: file.length,
+      sentence: file.sentence,
+    }));
+  }
+}
