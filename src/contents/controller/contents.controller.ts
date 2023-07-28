db13860cddc370a1dcb4e054bb25f0ee853b27e0
허지수 <heojisu@heojisuui-MacBookAir.local>
2023-07-28 16:23:15 +0900

    feat: 디테일 페이지

--- src/contents/controller/contents.controller.ts
+++ src/contents/controller/contents.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 import { ContentsService } from '../service/contents.service';
 
 @Controller('contents')
@@ -8,7 +8,7 @@ export class ContentsController {
   @Get()
   async getFilesByTopic(@Query('topic') topic: string) {
     const files = await this.contentsService.findByTopic(topic);
-    console.log('fr:', topic, files);
+    console.log('fr: ', topic, files);
     return files.map((file) => ({
       name: file.name,
       url: `https://d1gvhezy2xavcm.cloudfront.net/${file.name}_variant.m3u8`,
@@ -16,5 +16,20 @@ export class ContentsController {
       length: file.length,
       sentence: file.sentence,
     }));
+  }
+
+  @Get('/:name')
+  async getFileByName(@Param('name') name: string) {
+    const file = await this.contentsService.findByName(name);
+    const data = {
+      name: file.name,
+      originalName: file.originalName,
+      url: `https://d1gvhezy2xavcm.cloudfront.net/${file.name}_variant.m3u8`,
+      length: file.length,
+      topic: file.topic,
+      sentence: file.sentence,
+    };
+    console.log(data);
+    return data;
   }
 }
